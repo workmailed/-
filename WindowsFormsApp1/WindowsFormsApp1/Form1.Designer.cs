9d8e0f058d1c6a8230d87a2ecc031d34322e9b06
凯 郭 <gggk111@outlook.com>
2017-07-10 11:47:30 +0800

    添加项目

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeList1 = new DevExpress.XtraTreeList.TreeList();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.treeList1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeList1
+            // 
+            this.treeList1.Location = new System.Drawing.Point(12, 27);
+            this.treeList1.Name = "treeList1";
+            this.treeList1.SelectImageList = this.imageList1;
+            this.treeList1.Size = new System.Drawing.Size(522, 202);
+            this.treeList1.TabIndex = 5;
+            this.treeList1.GetSelectImage += new DevExpress.XtraTreeList.GetSelectImageEventHandler(this.treeList1_GetSelectImage);
+            this.treeList1.AfterFocusNode += new DevExpress.XtraTreeList.NodeEventHandler(this.treeList1_AfterFocusNode);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "add.png");
+            this.imageList1.Images.SetKeyName(1, "applications.png");
+            this.imageList1.Images.SetKeyName(2, "Apply_16x16.png");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 263);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 35);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "添加数据";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(153, 263);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 35);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "清空父节点";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(294, 261);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 35);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "设定值";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(435, 259);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 35);
+            this.button4.TabIndex = 25;
+            this.button4.Text = "删除指定节点";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 329);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(99, 35);
+            this.button5.TabIndex = 26;
+            this.button5.Text = "数据库添加条目";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(153, 329);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(99, 35);
+            this.button6.TabIndex = 27;
+            this.button6.Text = "数据库查询";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 397);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeList1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.treeList1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DevExpress.XtraTreeList.TreeList treeList1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,134 @@
+﻿using DevExpress.XtraTreeList.Nodes;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        DataTable dt = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+
+            dt.Columns.Add("ID", typeof(int));
+            dt.Columns.Add("ParentID", typeof(int));
+            dt.Columns.Add("Name", typeof(string));
+            dt.Columns.Add("Number", typeof(string));
+            dt.Columns.Add("W/M", typeof(string));
+
+            treeList1.KeyFieldName = "ID";
+            treeList1.ParentFieldName = "ParentID";
+            treeList1.DataSource = dt;
+
+            dt.Rows.Add(1, 0, "山东", 1);
+            dt.Rows.Add(12, 0, "西安", 12);
+        }
+        #region  建立MySql数据库连接
+        /// <summary>
+        /// 建立数据库连接.
+        /// </summary>
+        /// <returns>返回MySqlConnection对象</returns>
+        public MySqlConnection getmysqlcon()
+        {
+            string M_str_sqlcon = "server=localhost;user=root;database=world;port=3306;password=Gk280014"; //根据自己的设置
+            MySqlConnection myCon = new MySqlConnection(M_str_sqlcon);
+            return myCon;
+        }
+        #endregion
+        #region  执行MySqlCommand命令
+        /// <summary>
+        /// 执行MySqlCommand
+        /// </summary>
+        /// <param name="M_str_sqlstr">SQL语句</param>
+        public void getmysqlcom(string M_str_sqlstr)
+        {
+            MySqlConnection mysqlcon = this.getmysqlcon();
+            mysqlcon.Open();
+            MySqlCommand mysqlcom = new MySqlCommand(M_str_sqlstr, mysqlcon);
+            mysqlcom.ExecuteNonQuery();
+            mysqlcom.Dispose();
+            mysqlcon.Close();
+            mysqlcon.Dispose();
+        }
+        #endregion
+        #region  创建MySqlDataReader对象
+        /// <summary>
+        /// 创建一个MySqlDataReader对象
+        /// </summary>
+        /// <param name="M_str_sqlstr">SQL语句</param>
+        /// <returns>返回MySqlDataReader对象</returns>
+        public MySqlDataReader getmysqlread(string M_str_sqlstr)
+        {
+            MySqlConnection mysqlcon = this.getmysqlcon();
+            MySqlCommand mysqlcom = new MySqlCommand(M_str_sqlstr, mysqlcon);
+            mysqlcon.Open();
+            MySqlDataReader mysqlread = mysqlcom.ExecuteReader(CommandBehavior.CloseConnection);
+            return mysqlread;
+        }
+        #endregion
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+
+            dt.Rows.Add(2, 1, "济南", 1);//添加数据
+            dt.Rows.Add(1, 2, "济南", 1);
+            dt.Rows.Add(3, 1, "泰安", 1);
+            dt.Rows.Add(4, 1, "济宁", 1);
+            dt.Rows.Add(5, 1, "菏泽", 1);
+
+            dt.Rows.Add(1, 12, "济南", 2);
+            dt.Rows.Add(2, 12, "泰安", 2);
+            dt.Rows.Add(3, 12, "济宁", 2);
+            dt.Rows.Add(4, 12, "菏泽", 2);
+        }
+        TreeListNode deletenode;
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            treeList1.DeleteNode(treeList1.Nodes[0]);//删除父节点
+        }
+
+        private void treeList1_AfterFocusNode(object sender, DevExpress.XtraTreeList.NodeEventArgs e)
+        {
+            deletenode = e.Node; //获取最后的ID
+        }
+
+        private void button3_Click(object sender, System.EventArgs e)
+        {
+            treeList1.Nodes[0].Nodes[0].SetValue(0, "1111111");//改值
+        }
+
+        private void button4_Click(object sender, System.EventArgs e)
+        {
+            treeList1.DeleteNode(deletenode);//删除选中节点
+        }
+
+        private void treeList1_GetSelectImage(object sender, DevExpress.XtraTreeList.GetSelectImageEventArgs e)
+        {
+            if (e.Node == null) return;
+            TreeListNode node = e.Node;
+
+            int ID = (int)node.GetValue("ID");
+            if (ID == 1)
+                e.NodeImageIndex = 0;
+            else
+                e.NodeImageIndex = 2;
+        }
+
+        private void button5_Click(object sender, System.EventArgs e)
+        {
+            getmysqlcom("insert into blog(ID,name) values('2','abc')");
+        }
+
+        private void button6_Click(object sender, System.EventArgs e)
+        {
+            int i = 0;
+            MySqlDataReader mysqlread = getmysqlread("select * from blog");
+            while (mysqlread.Read())
+            {
+                treeList1.Nodes[0].Nodes[i].SetValue(0, mysqlread.GetString(1));
+                i++;
+            }
+
+        }
+    }
+}
